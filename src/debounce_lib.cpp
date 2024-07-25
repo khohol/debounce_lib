7e28028032bea1c4de3bb86928f635d2c778b150
Yaroslav Ts <98484361+khohol@users.noreply.github.com>
2024-07-25 19:28:46 +0300

    new file:   src/debounce_lib.cpp 	new file:   src/debounce_lib.h

---
+++ src/debounce_lib.cpp
@@ -0,0 +1,59 @@
+#include "debounce_lib.h"
+#include <Arduino.h>
+
+
+// КОНСТРУКТОР
+debouncer::debouncer() {
+}
+
+
+// СЕТТЕРИ
+void debouncer::setDebounce(uint16_t new_debounce) {
+    _debounce = new_debounce;
+}
+
+void debouncer::setColdown(uint16_t new_coldown) {
+    _coldown = new_coldown;
+}
+
+
+// ОБРОБКА
+bool debouncer::tick() {
+    if (_last_value != _value) {
+        _debounce_timer = millis();
+        _debounce_flag = true;
+    }
+
+    if (_debounce_flag && millis() - _debounce_timer >= _debounce && millis() - _coldown_timer >= _coldown) {
+        _debounce_flag = false;
+        _coldown_timer = millis();
+        _isTrig_flag = true;
+        _debounced_value = _value;
+    }
+
+    _last_value = _value;
+    return true;
+}
+
+
+bool debouncer::isTrig() {
+    debouncer::tick();
+    if (_isTrig_flag) {
+        _isTrig_flag = false;
+        return true;
+    } else return false;
+};
+
+
+int16_t debouncer::setValue(int16_t input_value) {
+    input_value = _value;
+    debouncer::tick();
+}
+
+
+int16_t debouncer::getValue() {
+    debouncer::tick();
+    if (_debounced_value == -1) {
+        return _value;
+    } else return _debounced_value;
+}
---
+++ src/debounce_lib.h
@@ -0,0 +1,30 @@
+#ifndef debounce_lib_h
+#define debounce_lib_h
+
+class debouncer {
+    public:
+        void setDebounce(uint16_t new_debounce);
+        void setColdown(uint16_t new_coldown);
+        bool tick();
+        
+        int16_t setValue(int16_t input_value);
+        int16_t getValue();
+        bool isTrig();
+        //bool isHolded();
+    private:
+        uint16_t _debounce  = 300;
+        uint32_t _debounce_timer = 0;
+        bool _debounce_flag = false;
+
+        uint16_t _coldown   = 800;
+        uint32_t _coldown_timer = 0;
+        bool _coldown_flag = false;
+        
+        int16_t _value = -1;
+        int16_t _debounced_value = _value;
+        int16_t _last_value = _value; 
+
+        bool _isTrig_flag = false;
+}
+
+#endif
