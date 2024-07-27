396103c4b50bc149e5ec8112c5845ca3e1874a35
Yaroslav Ts <98484361+khohol@users.noreply.github.com>
2024-07-27 17:06:39 +0300

    modified:   src/debounce_lib.cpp 	modified:   src/debounce_lib.h

--- src/debounce_lib.h
+++ src/debounce_lib.h
@@ -2,25 +2,25 @@
 #define debounce_lib_h
 #include <Arduino.h>
 
-class debouncer {
+class Debouncer {
     public:
-        debouncer();
-        void setDebounce(uint16_t new_debounce);
-        void setColdown(uint16_t new_coldown);
-        bool tick();
+        Debouncer();
+        void setDebounce(uint16_t new_debounce);    // задання часу ігнорування дріб'язку значень
+        void setCooldown(uint16_t new_cooldown);    // задання часу "перезарядки"
+        bool tick();                                // викликати як можна частіше
         
-        bool setValue(int16_t input_value);
-        int16_t getValue();
-        bool isTrig();
+        bool setValue(int16_t input_value);         // задаємо значення з певною частотою
+        int16_t getValue();                         // отримання актуальних значень
+        bool isTrig();                              // повертає true, якщо була зміна значення
         //bool isHolded();
     private:
         uint16_t _debounce  = 300;
         uint32_t _debounce_timer = 0;
         bool _debounce_flag = false;
 
-        uint16_t _coldown   = 800;
-        uint32_t _coldown_timer = 0;
-        bool _coldown_flag = false;
+        uint16_t _cooldown   = 800;
+        uint32_t _cooldown_timer = 0;
+        bool _cooldown_flag = false;
         
         int16_t _value = -1;
         int16_t _debounced_value = _value;
